bec4563824b74df893088dab8d5b14a327b6a700
Sourav Kundu <Souravkundu98@gmail.com>
2017-01-24 08:51:54 +0530

    3 new puzzles added

---
+++ Nodes_Without_Sibling_BinaryTree/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nodes_Without_Sibling_BinaryTree
+{
+    class Program
+    {
+        static Node root;
+        static void Main(string[] args)
+        {
+            root = new Node(1);
+            root.left = new Node(2);
+            root.right = new Node(3);
+            root.left.right = new Node(4);
+            root.right.left = new Node(5);
+            root.right.left.right = new Node(6);
+            printSingles(root);
+        }
+
+        // Function to print all non-root nodes that don't have a sibling
+        static void printSingles(Node node)
+        {
+            // Base case
+            if (node == null)
+                return;
+
+            // If this is an internal node, recur for left
+            // and right subtrees
+            if (node.left != null && node.right != null)
+            {
+                printSingles(node.left);
+                printSingles(node.right);
+            }
+
+            // If left child is NULL and right is not, print right child
+            // and recur for right child
+            else if (node.right != null)
+            {
+                Console.Write(node.right.data + " ");
+                printSingles(node.right);
+            }
+
+            // If right child is NULL and left is not, print left child
+            // and recur for left child
+            else if (node.left != null)
+            {
+                Console.Write(node.left.data + " ");
+                printSingles(node.left);
+            }
+        }
+
+    }
+
+    // A binary tree node
+    class Node
+    {
+        public int data;
+        public Node left, right;
+
+        public Node(int item)
+        {
+            data = item;
+            left = right = null;
+        }
+    }
+}
+//http://www.geeksforgeeks.org/print-nodes-dont-sibling-binary-tree/
---
+++ Reverse_LinkedList/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_LinkedList
+{
+    class Program
+    {
+        static Node head;
+        static void Main(string[] args)
+        {
+            head = new Node(85);
+            head.next = new Node(15);
+            head.next.next = new Node(4);
+            head.next.next.next = new Node(20);
+
+            Console.WriteLine("Given Linked list");
+            printList(head);
+            head = reverse(head);
+            Console.WriteLine("");
+            Console.WriteLine("Reversed linked list ");
+            printList(head);
+        }
+
+        /* Function to reverse the linked list */
+        static public Node reverse(Node node)
+        {
+            Node prev = null;
+            Node current = node;
+            Node next = null;
+            while (current != null)
+            {
+                next = current.next;
+                current.next = prev;
+                prev = current;
+                current = next;
+            }
+            node = prev;
+            return node;
+        }
+
+        // prints content of double linked list
+        static public void printList(Node node)
+        {
+            while (node != null)
+            {
+                Console.Write(node.data + " ");
+                node = node.next;
+            }
+        }
+    }
+
+    class Node
+    {
+
+        public int data;
+        public Node next;
+
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+}
+
+
+//http://www.geeksforgeeks.org/write-a-function-to-reverse-the-nodes-of-a-linked-list/
